e7470a15502917927dcc444e8697f0fb9a58114f
nezbednik <shopphoto504@gmail.com>
2022-09-15 11:11:04 +0200

    add source

---
+++ package/base-files-prebuild/files/staging_dir/target-mips_r2_uClibc-0.9.33.2/usr/include/zcfg_feature_flag.h
@@ -0,0 +1,7 @@
+
+/* Device.X_ZYXEL_FEATRUE_FLAG.Flag */
+#define EOGRE_TUNNEL                                   (1 << 0)
+#define ZYCFGFILTER_RESTORE_WITHOUT_CHANGE_ACS_INFO    (1 << 1)
+#define HIDE_WLANGUESTAP_TAP						   (1 << 2)
+#define CUSTOMIZATION_LOGO_ZAP                         (1 << 3)
+#define WIZARD_CCWAN                         		   (1 << 4)
